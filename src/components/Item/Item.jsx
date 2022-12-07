6949652f138c019432f66e26d73c7395fe91ee77
Ruslantarget <ruslan.bulatov.96@gmail.com>
2022-12-07 17:31:10 +0300

    fixed checkbox bug

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,15 +5,14 @@ function Item( { item, setAllLists, listId, allLists } ) {
 
     return (
         <div className="item">
-            <input className='item__checkbox' type="checkbox" id='item' checked={item.checked} 
+            <input className='item__checkbox' type="checkbox" id={`item-${item.id}`} checked={item.checked} 
             onChange={()=>{
                 setAllLists(
-                    allLists.map((list)=>(list.id === listId ? {...list, items: list.items.map((itemMap) => {   console.log(itemMap);
-                        console.log(item);
+                    allLists.map((list)=>(list.id === listId ? {...list, items: list.items.map((itemMap) => {
                         return (itemMap.id === item.id ? {...itemMap, checked : !itemMap.checked} : itemMap)})} : list))
                 ) 
             }}/>
-            <label className={`item__description ${item.checked ? 'item__description_completed' : ''}`} for="item">{item.title}</label>
+            <label className={`item__description ${item.checked ? 'item__description_completed' : ''}`} for={`item-${item.id}`}>{item.title}</label>
         </div>
     );
 }
